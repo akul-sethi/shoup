2edea98605cc59d966df55c77ccb4a6c95f89859
Akul Sethi <sethi.ak@northeastern.edu>
2023-05-11 18:10:10 -0400

    Initial Commit

---
+++ Shoup/ShoupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoupApp.swift
+//  Shoup
+//
+//  Created by Akul Sethi on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
